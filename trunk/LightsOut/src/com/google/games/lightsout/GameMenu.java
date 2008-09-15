56129d128215adf464a805ead4abe0a392dc4588
sjmccarthy <sjmccarthy@0669e46b-4956-0410-ba27-59b1c40b4850>
2008-09-15 14:26:56 +0000

    Initial import

---
+++ trunk/LightsOut/src/com/google/games/lightsout/GameMenu.java
@@ -0,0 +1,31 @@
+package com.google.games.lightsout;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GameMenu extends Activity {
+  
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      
+      
+      setContentView(R.layout.game_menu);
+      
+      Button newGameButton = (Button) findViewById(R.id.new_game);
+      
+      final Activity a = this;
+      newGameButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+          Intent intent = new Intent(a, GamePlay.class);
+          startActivity(intent);
+        }
+      });
+  }
+}
