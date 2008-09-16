8143323405cca9943c079ff297b6a10e5faa26c8
sjmccarthy <sjmccarthy@0669e46b-4956-0410-ba27-59b1c40b4850>
2008-09-16 05:40:14 +0000

    Adds game menu and auto-game saving on-exit.

--- trunk/LightsOut/src/com/google/games/lightsout/GameMenu.java
+++ trunk/LightsOut/src/com/google/games/lightsout/GameMenu.java
@@ -1,6 +1,10 @@
 package com.google.games.lightsout;
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,6 +23,7 @@ public class GameMenu extends Activity {
       setContentView(R.layout.game_menu);
       
       Button newGameButton = (Button) findViewById(R.id.new_game);
+      Button continueGameButton = (Button) findViewById(R.id.continue_game);
       
       final Activity a = this;
       newGameButton.setOnClickListener(new View.OnClickListener() {
@@ -27,5 +32,32 @@ public class GameMenu extends Activity {
           startActivity(intent);
         }
       });
+      
+      if (doesSavedGameExist()) {
+        continueGameButton.setEnabled(true);
+        continueGameButton.setOnClickListener(new View.OnClickListener() {
+          public void onClick(View v) {
+            Intent intent = new Intent(a, GamePlay.class);
+            intent.putExtra(GamePlay.CONTINUE_GAME, true);
+            startActivity(intent);
+          }
+        });
+      }
+      
+      
+  }
+  
+  private boolean doesSavedGameExist() {
+    try {
+      FileInputStream is = this.openFileInput(GamePlay.SAVE_FILE_NAME);
+      if (is.read() != 1) {
+        return true;
+      }
+    } catch (FileNotFoundException e) {
+    } catch (IOException e) {
+      // Do nothing, any exceptions just return false
+    }
+    
+    return false;
   }
 }
