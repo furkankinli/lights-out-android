0fc53c8d7a4c36c827c694ba5eb07636d1773751
sjmccarthy <sjmccarthy@0669e46b-4956-0410-ba27-59b1c40b4850>
2008-10-02 18:42:44 +0000

    doesn't work

--- LightsOut/src/com/google/games/lightsout/GameMenu.java
+++ LightsOut/src/com/google/games/lightsout/GameMenu.java
@@ -1,41 +1,36 @@
 package com.google.games.lightsout;
 
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class GameMenu extends Activity {
   
-  private static final int GAME_PLAY = 0;
-  
-  private HighScoreManager highScoreManager;
+  public static final String HIGH_SCORE_MANAGER = "com.google.game.lightsout.HighScoreManager";
   
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       
-      highScoreManager = new HighScoreManager(this);
-      
       setContentView(R.layout.game_menu);
       
-      Button newGameButton = (Button) findViewById(R.id.new_game);
-      Button continueGameButton = (Button) findViewById(R.id.continue_game);
+      Button newGameButton = (Button) findViewById(R.id.new_game_button);
+      Button continueGameButton = (Button) findViewById(R.id.continue_game_button);
+      Button highScoresGameButton = (Button) findViewById(R.id.high_scores_button);
       
-      final Activity a = this;
+      final Intent intent = new Intent(this, GamePlay.class);
       newGameButton.setOnClickListener(new View.OnClickListener() {
         public void onClick(View v) {
-          Intent intent = new Intent(a, GamePlay.class);
           intent.putExtra(GamePlay.NEW_GAME, true);
-          startActivityForResult(intent, GAME_PLAY);
+          intent.putExtra(GamePlay.HIGH_SCORES, false);
+          startActivity(intent);
         }
       });
       
@@ -43,37 +38,45 @@ public class GameMenu extends Activity {
         continueGameButton.setEnabled(true);
         continueGameButton.setOnClickListener(new View.OnClickListener() {
           public void onClick(View v) {
-            Intent intent = new Intent(a, GamePlay.class);
-            startActivityForResult(intent, GAME_PLAY);
+            intent.putExtra(GamePlay.NEW_GAME, false);
+            intent.putExtra(GamePlay.HIGH_SCORES, false);
+            startActivity(intent);
           }
         });
       }
-      
-      
+      highScoresGameButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+          intent.putExtra(GamePlay.NEW_GAME, false);
+          intent.putExtra(GamePlay.HIGH_SCORES, true);
+          startActivity(intent);
+        }
+      });
   }
   
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-    super.onActivityResult(requestCode, resultCode, intent);
-    if (requestCode == GAME_PLAY && intent != null) {
-      Bundle extras = intent.getExtras();
-      int totalSeconds = extras.getInt(GamePlay.TOTAL_TIME);
-      int totalMoves = extras.getInt(GamePlay.TOTAL_MOVES);
-      if (highScoreManager.isHighScore(totalSeconds, totalMoves)) {
-        final Dialog namePromptDialog = new Dialog(this);
-        namePromptDialog.setContentView(R.layout.name_prompt_dialog);
-        namePromptDialog.setTitle(R.string.new_high_score);
-        Button button = (Button) namePromptDialog.findViewById(R.id.ok);
-        button.setOnClickListener(new OnClickListener() {
-          public void onClick(View v) {
-            namePromptDialog.dismiss();
-          }
-        });
-        
-        namePromptDialog.show();
-      }
-    }
-  }
+  
+  
+//  @Override
+//  protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//    super.onActivityResult(requestCode, resultCode, intent);
+//    if (requestCode == GAME_PLAY && intent != null) {
+//      Bundle extras = intent.getExtras();
+//      int totalSeconds = extras.getInt(GamePlay.TOTAL_TIME);
+//      int totalMoves = extras.getInt(GamePlay.TOTAL_MOVES);
+//      if (highScoreManager.isHighScore(totalSeconds, totalMoves)) {
+//        final Dialog namePromptDialog = new Dialog(this);
+//        namePromptDialog.setContentView(R.layout.name_prompt_dialog);
+//        namePromptDialog.setTitle(R.string.new_high_score);
+//        Button button = (Button) namePromptDialog.findViewById(R.id.ok);
+//        button.setOnClickListener(new OnClickListener() {
+//          public void onClick(View v) {
+//            namePromptDialog.dismiss();
+//          }
+//        });
+//        
+//        namePromptDialog.show();
+//      }
+//    }
+//  }
   
   private boolean doesSavedGameExist() {
     try {
